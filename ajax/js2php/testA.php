8b6d35b1d41cb55d35f48c470c8df45df66afd8e
jonnypeng <jonny.peng@qq.com>
2016-07-29 13:15:41 +0800

    bind

---
+++ ajax/js2php/testA.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title></title>
+</head>
+<body>
+<?php 
+		$result=10001;
+?>
+	<a href="testB.php?newid=<?php echo $result; ?>"></a>
+</body>
+</html>
